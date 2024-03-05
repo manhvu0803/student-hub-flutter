ac369e6551f8bb1ea41d47ce4682ccfab374f347
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2024-03-06 00:34:37 +0700

    Add home page

---
+++ lib/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 64),
+              Text(
+                'Build your product with high skilled student',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold)
+              ),
+              const SizedBox(height: 32),
+              Text(
+                "StudentHub is university market place to connect high-skilled student and company on a real-world project",
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 32),
+              const _UserTypeButton(Icons.person, "Student"),
+              const _UserTypeButton(Icons.apartment, "Company"),
+              const SizedBox(height: 32),
+              Text(
+                "Find and onboard best-skilled student for your product. Student works to gain experience & skills from real-world projects",
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _UserTypeButton extends StatelessWidget {
+  final IconData icon;
+
+  final String text;
+
+  const _UserTypeButton(this.icon, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).secondaryHeaderColor),
+        child: SizedBox(
+          height: 60,
+          width: 180,
+          child: ListTile(
+            leading: Icon(icon),
+            title: Text(text),
+          ),
+        )
+      ),
+    );
+  }
+}
