7b593eac487a30af153fd876b3666f46f553a7c3
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2024-03-07 11:17:11 +0700

    feat: Screens division

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+
+/* 
+column 
+  active account
+  expandable (other accounts)
+    other acc1
+    acc2
+
+  Seperator
+
+  column
+    profile
+    settings
+    logout
+*/
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(children: <Widget> [
+
+
+    ],);
+  }
+}
