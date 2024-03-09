fa2c4097e3d5b8d37b4d22bbe4bc2cd9305373b1
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2024-03-09 11:04:39 +0700

    feat: base elements layout

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
-
 /* 
 column 
   active account
@@ -21,9 +20,49 @@ column
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: <Widget> [
-
-
-    ],);
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text("current acc"),
+          subtitle: Text("Company"),
+          onTap: () {},
+        ),
+        ExpansionTile(
+          title: Text("other accc"),
+          children: <Widget>[
+            ListTile(
+              title: Text("acc1"),
+              subtitle: Text("Student"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("acc2"),
+              subtitle: Text("Student"),
+              onTap: () {},
+            )
+          ],
+        ),
+        const Divider(
+          height: 10,
+        ),
+        Column(
+          children: <Widget>[
+            ListTile(
+              title: const Text("Profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Logout"),
+              onTap: () {},
+              tileColor: Colors.red,
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
