6d5f37c91358481a3dcfbb92028b5213565e048a
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2024-04-12 14:07:56 +0700

    feat: Add company sign up and fix profile page

--- lib/screens/Profile/profile_page.dart
+++ lib/screens/Profile/profile_page.dart
@@ -7,8 +7,8 @@ import 'package:student_hub_flutter/screens/Profile/student_profile_basic.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
-/* 
-column 
+/*
+column
   active account
   expandable (other accounts)
     other acc1
@@ -24,81 +24,83 @@ column
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: FractionallySizedBox(
-            widthFactor: 0.9,
-            heightFactor: .9,
-            child: Column(
-              children: <Widget>[
-                ListTile(
-                  title: Text("current acc",
-                      style: Theme.of(context).textTheme.bodyMedium),
-                  subtitle: Text("Company",
-                      style: Theme.of(context).textTheme.bodySmall),
-                  onTap: () {},
-                ),
-                ExpansionTile(
-                  title: Text("other accc",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium
-                          ?.copyWith(fontWeight: FontWeight.bold)),
-                  children: <Widget>[
-                    ListTile(
-                      title: Text("acc1",
-                          style: Theme.of(context).textTheme.bodyMedium),
-                      subtitle: Text("Student",
-                          style: Theme.of(context).textTheme.bodySmall),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: Text("acc2",
-                          style: Theme.of(context).textTheme.bodyMedium),
-                      subtitle: Text("Student",
-                          style: Theme.of(context).textTheme.bodySmall),
-                      onTap: () {},
-                    )
-                  ],
-                ),
-                const Divider(
-                  height: 10,
-                ),
-                Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text("Profile",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(fontWeight: FontWeight.bold)),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          // return CompanyProfile();
-                          return const StudentProfileBasic();
-                        }));
-                      },
-                    ),
-                    ListTile(
-                      title: Text("Settings",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(fontWeight: FontWeight.bold)),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: Text("Logout",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(fontWeight: FontWeight.bold)),
-                      onTap: () {},
-                      tileColor: Colors.red,
-                    ),
-                  ],
-                )
-              ],
-            )));
+    return Scaffold(
+      body: Center(
+          child: FractionallySizedBox(
+              widthFactor: 0.9,
+              heightFactor: .9,
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text("current acc",
+                        style: Theme.of(context).textTheme.bodyMedium),
+                    subtitle: Text("Company",
+                        style: Theme.of(context).textTheme.bodySmall),
+                    onTap: () {},
+                  ),
+                  ExpansionTile(
+                    title: Text("other accc",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(fontWeight: FontWeight.bold)),
+                    children: <Widget>[
+                      ListTile(
+                        title: Text("acc1",
+                            style: Theme.of(context).textTheme.bodyMedium),
+                        subtitle: Text("Student",
+                            style: Theme.of(context).textTheme.bodySmall),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: Text("acc2",
+                            style: Theme.of(context).textTheme.bodyMedium),
+                        subtitle: Text("Student",
+                            style: Theme.of(context).textTheme.bodySmall),
+                        onTap: () {},
+                      )
+                    ],
+                  ),
+                  const Divider(
+                    height: 10,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text("Profile",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(fontWeight: FontWeight.bold)),
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            // return CompanyProfile();
+                            return const StudentProfileBasic();
+                          }));
+                        },
+                      ),
+                      ListTile(
+                        title: Text("Settings",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(fontWeight: FontWeight.bold)),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: Text("Logout",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(fontWeight: FontWeight.bold)),
+                        onTap: () {},
+                        tileColor: Colors.red,
+                      ),
+                    ],
+                  )
+                ],
+              ))),
+    );
   }
 }
---
+++ lib/screens/pages/sign_up/student_sign_up_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:student_hub_flutter/extensions/context_dialog_extension.dart';
+import 'package:student_hub_flutter/extensions/context_theme_extension.dart';
+import 'package:student_hub_flutter/screens/pages/home_page.dart';
+import 'package:student_hub_flutter/widgets/extra_option_container.dart';
+import 'package:student_hub_flutter/widgets/page_screen.dart';
+
+import 'company_sign_up_page.dart';
+import 'sign_up_info_container.dart';
+
+class StudentSignUpPage extends StatelessWidget {
+  const StudentSignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageScreen(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 24),
+              Text(
+                "Signing up for students",
+                style: context.textTheme.headlineSmall!.copyWith(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 24),
+              SignUpInfoContainer(
+                onCreateAccount: () => context.pushRoute((context) => const HomePage(isStudentUser: true)),
+              ),
+              const SizedBox(height: 165),
+              BottomExtraOption.materialRoute(
+                text: "Looking for students?",
+                buttonText: "Sign up for companies",
+                builder: (context) => const CompanySignUpPage()
+              )
+            ]
+          )
+        )
+      )
+    );
+  }
+}
